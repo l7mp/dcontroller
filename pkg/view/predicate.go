60072d844bb97251bf7efde3a4c1b4537f469d8a
Gabor Retvari <retvari@tmit.bme.hu>
2024-09-14 16:52:50 +0200

    refactor: Simplify predicate parsing

--- pkg/view/predicate.go
+++ pkg/view/predicate.go
@@ -5,40 +5,41 @@ import (
 	"errors"
 	"fmt"
 
-	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
 var _ Interface = &Predicate{}
-
-// var _ json.Marshaler = &Predicate{}
-// var _ json.Unmarshaler = &Predicate{}
+var _ json.Marshaler = &Predicate{}
+var _ json.Unmarshaler = &Predicate{}
 
 type Interface interface {
 	ToPredicate() (predicate.Predicate, error)
 }
 
+type BasicPredicate string
+type BoolPredicate map[string]([]Predicate)
+
 type Predicate struct {
-	basic *BasicPredicate
-	boolp *BoolPredicate
+	*BasicPredicate
+	*BoolPredicate
 }
 
 func (p *Predicate) ToPredicate() (predicate.Predicate, error) {
-	if p.basic != nil {
-		return p.basic.ToPredicate()
+	if p.BasicPredicate != nil {
+		return p.BasicPredicate.ToPredicate()
 	}
-	if p.boolp != nil {
-		return p.boolp.ToPredicate()
+	if p.BoolPredicate != nil {
+		return p.BoolPredicate.ToPredicate()
 	}
 	return nil, errors.New("invalid predicate")
 }
 
 func (p Predicate) MarshalJSON() ([]byte, error) {
-	if p.basic != nil {
-		return json.Marshal(p.basic)
+	if p.BasicPredicate != nil {
+		return json.Marshal(p.BasicPredicate)
 	}
-	if p.boolp != nil {
-		return json.Marshal(p.boolp)
+	if p.BoolPredicate != nil {
+		return json.Marshal(p.BoolPredicate)
 	}
 	return nil, errors.New("invalid predicate")
 }
@@ -48,7 +49,7 @@ func (p *Predicate) UnmarshalJSON(data []byte) error {
 	var pw BasicPredicate
 	err := json.Unmarshal(data, &pw)
 	if err == nil {
-		*p = Predicate{basic: &pw}
+		*p = Predicate{BasicPredicate: &pw}
 		return nil
 	}
 
@@ -56,117 +57,92 @@ func (p *Predicate) UnmarshalJSON(data []byte) error {
 	var bp BoolPredicate
 	err = json.Unmarshal(data, &bp)
 	if err == nil {
-		*p = Predicate{boolp: &bp}
+		*p = Predicate{BoolPredicate: &bp}
 		return nil
 	}
 
 	return err
 }
 
-//////////////
-
-type BasicPredicate struct {
-	Type    *string `json:"type"`
-	TypeArg *map[string]string
-}
-
-type BoolPredicate struct {
-	Type       string           `json:"type"`
-	Predicates []BasicPredicate `json:"predicates"`
-}
-
 func (pw *BasicPredicate) ToPredicate() (predicate.Predicate, error) {
-	if pw.Type != nil {
-		switch *pw.Type {
-		case "GenerationChanged":
-			return predicate.GenerationChangedPredicate{}, nil
-		case "ResourceVersionChanged":
-			return predicate.ResourceVersionChangedPredicate{}, nil
-		case "LabelChanged":
-			return predicate.LabelChangedPredicate{}, nil
-		case "AnnotationChanged":
-			return predicate.AnnotationChangedPredicate{}, nil
-		default:
-			return nil, fmt.Errorf("unknown predicate type: %s", *pw.Type)
-		}
+	switch string(*pw) {
+	case "GenerationChanged":
+		return predicate.GenerationChangedPredicate{}, nil
+	case "ResourceVersionChanged":
+		return predicate.ResourceVersionChangedPredicate{}, nil
+	case "LabelChanged":
+		return predicate.LabelChangedPredicate{}, nil
+	case "AnnotationChanged":
+		return predicate.AnnotationChangedPredicate{}, nil
+	default:
+		return nil, fmt.Errorf("unknown predicate type: %s", *pw)
 	}
-
-	if pw.TypeArg != nil {
-		if len(*pw.TypeArg) != 1 {
-			return nil, errors.New("expecting a single type-argument pair")
-		}
-
-		for k, v := range *pw.TypeArg {
-			switch k {
-			case "Namespace":
-				return predicate.NewPredicateFuncs(func(object client.Object) bool {
-					return object.GetNamespace() == v
-				}), nil
-			default:
-				return nil, fmt.Errorf("unknown predicate type: %s", k)
-			}
-		}
-	}
-
-	return nil, errors.New("invalid basic predicate")
 }
 
 func (cp *BoolPredicate) ToPredicate() (predicate.Predicate, error) {
-	predicates := make([]predicate.Predicate, len(cp.Predicates))
-	for i, pw := range cp.Predicates {
-		p, err := pw.ToPredicate()
-		if err != nil {
-			return nil, err
+	if len(*cp) != 1 {
+		return nil, errors.New("expecting a single predicate op")
+	}
+
+	for k, v := range *cp {
+		predicates := make([]predicate.Predicate, len(v))
+		for i, pw := range v {
+			p, err := pw.ToPredicate()
+			if err != nil {
+				return nil, err
+			}
+			predicates[i] = p
 		}
-		predicates[i] = p
-	}
 
-	switch cp.Type {
-	case "And":
-		return predicate.And(predicates...), nil
-	case "Or":
-		return predicate.Or(predicates...), nil
-	case "Not":
-		if len(predicates) != 1 {
-			return nil, errors.New("invalid arguments to Not predicate")
+		switch k {
+		case "And":
+			return predicate.And(predicates...), nil
+		case "Or":
+			return predicate.Or(predicates...), nil
+		case "Not":
+			if len(predicates) != 1 {
+				return nil, errors.New("invalid arguments to Not predicate")
+			}
+			return predicate.Not(predicates[0]), nil
+		default:
+			return nil, fmt.Errorf("unknown bool predicate type: %s", k)
 		}
-		return predicate.Not(predicates[0]), nil
-	default:
-		return nil, fmt.Errorf("unknown bool predicate type: %s", cp.Type)
-	}
-}
-
-// these to are useless but simplify testing
-func MarshalBasicPredicate(p predicate.Predicate) ([]byte, error) {
-	var pw BasicPredicate
-
-	switch p.(type) {
-	case predicate.GenerationChangedPredicate:
-		t := "GenerationChanged"
-		pw = BasicPredicate{Type: &t}
-	case predicate.ResourceVersionChangedPredicate:
-		t := "ResourceVersionChanged"
-		pw = BasicPredicate{Type: &t}
-	case predicate.LabelChangedPredicate:
-		t := "LabelChanged"
-		pw = BasicPredicate{Type: &t}
-	case predicate.AnnotationChangedPredicate:
-		t := "AnnotationChanged"
-		pw = BasicPredicate{Type: &t}
-	case predicate.Funcs:
-		return nil, errors.New("cannot parse predicate functions")
-	default:
-		return nil, fmt.Errorf("unsupported predicate type: %T", p)
 	}
 
-	return json.Marshal(pw)
+	return nil, errors.New("invalid bool predicate")
 }
 
-func UnmarshalPredicate(data []byte) (predicate.Predicate, error) {
-	var pw BasicPredicate
-	err := json.Unmarshal(data, &pw)
-	if err != nil {
-		return nil, err
-	}
-	return pw.ToPredicate()
-}
+// // these to are useless but simplify testing
+// func MarshalBasicPredicate(p predicate.Predicate) ([]byte, error) {
+// 	var pw BasicPredicate
+
+// 	switch p.(type) {
+// 	case predicate.GenerationChangedPredicate:
+// 		t := PredType("GenerationChanged")
+// 		pw = BasicPredicate{PredType: &t}
+// 	case predicate.ResourceVersionChangedPredicate:
+// 		t := PredType("ResourceVersionChanged")
+// 		pw = BasicPredicate{PredType: &t}
+// 	case predicate.LabelChangedPredicate:
+// 		t := PredType("LabelChanged")
+// 		pw = BasicPredicate{PredType: &t}
+// 	case predicate.AnnotationChangedPredicate:
+// 		t := PredType("AnnotationChanged")
+// 		pw = BasicPredicate{PredType: &t}
+// 	case predicate.Funcs:
+// 		return nil, errors.New("cannot parse predicate functions")
+// 	default:
+// 		return nil, fmt.Errorf("unsupported predicate type: %T", p)
+// 	}
+
+// 	return json.Marshal(pw)
+// }
+
+// func UnmarshalPredicate(data []byte) (predicate.Predicate, error) {
+// 	var pw BasicPredicate
+// 	err := json.Unmarshal(data, &pw)
+// 	if err != nil {
+// 		return nil, err
+// 	}
+// 	return pw.ToPredicate()
+// }
--- pkg/view/predicate_test.go
+++ pkg/view/predicate_test.go
@@ -11,174 +11,11 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/predicate"
 )
 
-var _ = Describe("Predicate Marshaling", func() {
-	Context("with standard predicates", func() {
+var _ = Describe("Predicate", func() {
+	Context("with simple predicates", func() {
 		It("should marshal and unmarshal GenerationChangedPredicate", func() {
-			pred := predicate.GenerationChangedPredicate{}
-			data, err := MarshalBasicPredicate(pred)
-			Expect(err).NotTo(HaveOccurred())
-
-			unmarshaledPred, err := UnmarshalPredicate(data)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(unmarshaledPred).To(BeAssignableToTypeOf(predicate.GenerationChangedPredicate{}))
-		})
-
-		It("should marshal and unmarshal ResourceVersionChangedPredicate", func() {
-			pred := predicate.ResourceVersionChangedPredicate{}
-			data, err := MarshalBasicPredicate(pred)
-			Expect(err).NotTo(HaveOccurred())
-
-			unmarshaledPred, err := UnmarshalPredicate(data)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(unmarshaledPred).To(BeAssignableToTypeOf(predicate.ResourceVersionChangedPredicate{}))
-		})
-
-		It("should marshal and unmarshal LabelChangedPredicate", func() {
-			pred := predicate.LabelChangedPredicate{}
-			data, err := MarshalBasicPredicate(pred)
-			Expect(err).NotTo(HaveOccurred())
-
-			unmarshaledPred, err := UnmarshalPredicate(data)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(unmarshaledPred).To(BeAssignableToTypeOf(predicate.LabelChangedPredicate{}))
-		})
-
-		It("should marshal and unmarshal AnnotationChangedPredicate", func() {
-			pred := predicate.AnnotationChangedPredicate{}
-			data, err := MarshalBasicPredicate(pred)
-			Expect(err).NotTo(HaveOccurred())
-
-			unmarshaledPred, err := UnmarshalPredicate(data)
-			Expect(err).NotTo(HaveOccurred())
-			Expect(unmarshaledPred).To(BeAssignableToTypeOf(predicate.AnnotationChangedPredicate{}))
-		})
-	})
-
-	Context("with composite predicates", func() {
-		It("should marshal and unmarshal And predicate", func() {
-			t1 := "GenerationChanged"
-			t2 := "ResourceVersionChanged"
-			compPred := BoolPredicate{
-				Type: "And",
-				Predicates: []BasicPredicate{
-					{Type: &t1},
-					{Type: &t2},
-				},
-			}
-
-			data, err := json.Marshal(compPred)
-			Expect(err).NotTo(HaveOccurred())
-
-			var unmarshaledCompPred BoolPredicate
-			err = json.Unmarshal(data, &unmarshaledCompPred)
-			Expect(err).NotTo(HaveOccurred())
-
-			reconstitutedPred, err := unmarshaledCompPred.ToPredicate()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(reconstitutedPred).To(BeAssignableToTypeOf(predicate.And[client.Object]()))
-		})
-
-		It("should marshal and unmarshal Or predicate", func() {
-			t1 := "LabelChanged"
-			t2 := "AnnotationChanged"
-			compPred := BoolPredicate{
-				Type: "Or",
-				Predicates: []BasicPredicate{
-					{Type: &t1},
-					{Type: &t2},
-				},
-			}
-
-			data, err := json.Marshal(compPred)
-			Expect(err).NotTo(HaveOccurred())
-
-			var unmarshaledCompPred BoolPredicate
-			err = json.Unmarshal(data, &unmarshaledCompPred)
-			Expect(err).NotTo(HaveOccurred())
-
-			reconstitutedPred, err := unmarshaledCompPred.ToPredicate()
-			Expect(err).NotTo(HaveOccurred())
-			Expect(reconstitutedPred).To(BeAssignableToTypeOf(predicate.Or[client.Object]()))
-		})
-	})
-
-	Context("predicate behavior", func() {
-		It("should correctly apply GenerationChangedPredicate", func() {
-			t := "GenerationChanged"
-			compPred := BasicPredicate{
-				Type: &t,
-			}
-
-			reconstitutedPred, err := compPred.ToPredicate()
-			Expect(err).NotTo(HaveOccurred())
-
-			oldObj := &unstructured.Unstructured{}
-			oldObj.SetGeneration(1)
-			newObj := &unstructured.Unstructured{}
-			newObj.SetGeneration(2)
-
-			Expect(reconstitutedPred.Create(event.CreateEvent{
-				Object: client.Object(oldObj),
-			})).To(BeTrue())
-			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObj),
-			})).To(BeTrue())
-			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(oldObj),
-			})).To(BeFalse())
-			Expect(reconstitutedPred.Delete(event.DeleteEvent{
-				Object: client.Object(oldObj),
-			})).To(BeTrue())
-			Expect(reconstitutedPred.Generic(event.GenericEvent{
-				Object: client.Object(oldObj)})).To(BeTrue())
-		})
-
-		It("should correctly apply composite And predicate", func() {
-			t1 := "GenerationChanged"
-			t2 := "LabelChanged"
-			compPred := BoolPredicate{
-				Type: "And",
-				Predicates: []BasicPredicate{
-					{Type: &t1},
-					{Type: &t2},
-				},
-			}
-
-			reconstitutedPred, err := compPred.ToPredicate()
-			Expect(err).NotTo(HaveOccurred())
-
-			oldObj := &unstructured.Unstructured{}
-			oldObj.SetGeneration(1)
-			oldObj.SetLabels(map[string]string{"foo": "bar"})
-
-			newObjGenChanged := &unstructured.Unstructured{}
-			newObjGenChanged.SetGeneration(2)
-			newObjGenChanged.SetLabels(map[string]string{"foo": "bar"})
-
-			newObjLabelChanged := &unstructured.Unstructured{}
-			newObjLabelChanged.SetGeneration(1)
-			newObjLabelChanged.SetLabels(map[string]string{"foo": "baz"})
-
-			newObjBothChanged := &unstructured.Unstructured{}
-			newObjBothChanged.SetGeneration(2)
-			newObjBothChanged.SetLabels(map[string]string{"foo": "baz"})
-
-			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjGenChanged),
-			})).To(BeFalse())
-			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjLabelChanged),
-			})).To(BeFalse())
-			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjBothChanged),
-			})).To(BeTrue())
-		})
-	})
-
-	Context("with actual predicates", func() {
-		It("should marshal and unmarshal GenerationChangedPredicate", func() {
-			t := "GenerationChanged"
-			pred := Predicate{basic: &BasicPredicate{Type: &t}}
+			t := BasicPredicate("GenerationChanged")
+			pred := Predicate{BasicPredicate: &t}
 
 			data, err := json.Marshal(pred)
 			Expect(err).NotTo(HaveOccurred())
@@ -193,17 +30,11 @@ var _ = Describe("Predicate Marshaling", func() {
 		})
 
 		It("should marshal and unmarshal an Or predicate", func() {
-			t1 := "GenerationChanged"
-			t2 := "ResourceVersionChanged"
-			compPred := Predicate{
-				boolp: &BoolPredicate{
-					Type: "Or",
-					Predicates: []BasicPredicate{
-						{Type: &t1},
-						{Type: &t2},
-					},
-				},
-			}
+			t1 := BasicPredicate("GenerationChanged")
+			t2 := BasicPredicate("ResourceVersionChanged")
+			compPred := BoolPredicate(map[string][]Predicate{
+				"Or": []Predicate{{BasicPredicate: &t1}, {BasicPredicate: &t2}},
+			})
 
 			data, err := json.Marshal(compPred)
 			Expect(err).NotTo(HaveOccurred())
@@ -222,11 +53,11 @@ var _ = Describe("Predicate Marshaling", func() {
 
 			newObjGenChanged := &unstructured.Unstructured{}
 			newObjGenChanged.SetGeneration(2)
-			oldObj.SetResourceVersion("a")
+			newObjGenChanged.SetResourceVersion("a")
 
 			newObjResourceVerChanged := &unstructured.Unstructured{}
 			newObjResourceVerChanged.SetGeneration(1)
-			oldObj.SetResourceVersion("b")
+			newObjResourceVerChanged.SetResourceVersion("b")
 
 			newObjBothChanged := &unstructured.Unstructured{}
 			newObjBothChanged.SetGeneration(2)
@@ -244,14 +75,10 @@ var _ = Describe("Predicate Marshaling", func() {
 		})
 
 		It("should marshal and unmarshal and apply a Namespace predicate", func() {
-			compPred := Predicate{
-				boolp: &BoolPredicate{
-					Type: "Not",
-					Predicates: []BasicPredicate{
-						{TypeArg: &map[string]string{"Namespace": "default"}},
-					},
-				},
-			}
+			t := BasicPredicate("GenerationChanged")
+			compPred := Predicate{BoolPredicate: &BoolPredicate{
+				"Not": []Predicate{{BasicPredicate: &t}},
+			}}
 
 			data, err := json.Marshal(compPred)
 			Expect(err).NotTo(HaveOccurred())
@@ -266,20 +93,20 @@ var _ = Describe("Predicate Marshaling", func() {
 				predicate.Not[client.Object](predicate.GenerationChangedPredicate{})))
 
 			oldObj := &unstructured.Unstructured{}
-			oldObj.SetNamespace("default")
+			oldObj.SetGeneration(1)
 
-			newObjNsChanged := &unstructured.Unstructured{}
-			newObjNsChanged.SetNamespace("prod")
+			newObjGenChanged := &unstructured.Unstructured{}
+			newObjGenChanged.SetGeneration(2)
 
-			newObjNsUnchanged := &unstructured.Unstructured{}
-			newObjNsUnchanged.SetNamespace("default")
+			newObjGenUnchanged := &unstructured.Unstructured{}
+			newObjGenUnchanged.SetGeneration(1)
 
 			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjNsChanged),
-			})).To(BeTrue())
-			Expect(reconstitutedPred.Update(event.UpdateEvent{
-				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjNsUnchanged),
+				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjGenChanged),
 			})).To(BeFalse())
+			Expect(reconstitutedPred.Update(event.UpdateEvent{
+				ObjectOld: client.Object(oldObj), ObjectNew: client.Object(newObjGenUnchanged),
+			})).To(BeTrue())
 		})
 	})
 })
